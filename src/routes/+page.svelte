209e78ce4cf24358cd8a5af6fa362fd0483f44db
bit-dream <61420422+bit-dream@users.noreply.github.com>
2022-12-11 17:49:55 -0500

    Added help startup message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,82 +5,85 @@ import createGraph from './transforms';
 import _ from 'lodash'
 import type { DbcData } from 'dbc-can/lib/dbc/types';
 import Dialog, { Title, Content, Actions } from '@smui/dialog';
-import Snackbar from '@smui/snackbar';
 import Button, {Label} from '@smui/button';
 
-
-let open = false;
-let dialogTitle = '';
-let dialogDescription = '';
-const nodeDoubleClickHandler = (e: any, d: any) => {
-    console.log(e,d)
-    dialogTitle = d.name;
-    if (d.obj.description) {
-        dialogDescription = d.obj.description;
-    } else {
-        dialogDescription = 'This node does not have a description'
-    }
-    open = true;
-}
-
-let snackbar: Snackbar;
-
-function closeHandler(e: CustomEvent<{ action: string }>) {
+let isStartup = true;
+let open: boolean = true;
+let dialogTitle = 'Getting Started';
+function dialogHandler(e: CustomEvent<{ action: string }>) {
     switch (e.detail.action) {
       case 'close':
         open = false
+        if (isStartup) {
+            isStartup = false;
+        }
         break;
     }
 }
 
 /** @type {import('./$types').PageData} */  export let data: DbcData;
 let graph = createGraph(data);
-console.log(graph)
-const selector = '#test'
-const sim = new Simulation(selector, graph, nodeDoubleClickHandler);
+const nodeDoubleClickHandler = (e: any, d: any) => {
+    dialogTitle = d.name;
+    open = true;
+}
+const sim = new Simulation('#DBC_SIMULATION', graph, nodeDoubleClickHandler);
 onMount(() => {
 
     sim.setWidth = window.innerWidth;
     sim.setHeight = window.innerHeight;
     sim.init();
     sim.documentResizeHandler(window);
-
-    snackbar.open
 })
-
 
 </script>
 
-<div id="test" style="height: 100vh; width: 100vw; display: block;"></div>
+<div id="DBC_SIMULATION" style="height: 100vh; width: 100vw; display: block;"></div>
 
 <Dialog
   bind:open
+  scrimClickAction=""
+  escapeKeyAction=""
   aria-labelledby="large-scroll-title"
   aria-describedby="large-scroll-content"
   surface$style="width: 850px; max-width: calc(100vw - 32px);"
-  on:SMUIDialog:closed={closeHandler}
+  on:SMUIDialog:closed={dialogHandler}
 >
   <Title id="large-scroll-title">{dialogTitle}</Title>
   <Content id="large-scroll-content">
-    {dialogDescription}
+    {#if isStartup}
+    <ul>
+        <li>
+            The displayed graph can be both panned and zoomed
+            <ol>
+                <li>
+                    To zoom the graph, simply use your mouse's scroll wheel (or the appropriate touch gesture for your device)
+                </li>
+                <li>
+                    To pan the graph, press and hold your mouse button on any area with out a node, and drag in the direction you wish to pan
+                </li>
+            </ol>
+        </li>
+        <li>
+            Double clicking on any node will bring up a dialog indicating various properties that were pulled in from the DBC file
+        </li>
+        <li>
+            Graph interactivity
+            <ol>
+                <li>
+                    You can drag any node on the graph. On release, the node will stick to the place it was dragged to
+                </li>
+                <li>
+                    You can unstick a dragged node by clicking on the node a single time
+                </li>
+            </ol>
+        </li>
+    </ul>
+    {/if}
   </Content>
   <Actions>
-    <Button action="accept">
+    <Button action="close">
       <Label>Done</Label>
     </Button>
   </Actions>
 </Dialog>
- 
-<Snackbar bind:this={snackbar}>
-    <Label>
-        <h2>Using this simulation</h2>
-        <ul>
-            <li>You can pan/zoom the graph by clicking and draging in any white space as well as using your scroll wheel to zoom.</li>
-            <li>You can drag any nodes on the graph. On release, the node will stick to that spot. To unstick the node, simply give it a single click.</li>
-        </ul>
-    </Label>
-    <Actions>
-     
-    </Actions>
-</Snackbar>
-   
